6dcff709c3946605672220d5b74e3f4bb0842f11
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-06-18 01:20:35 +0800

    Fix eslint lint errors

--- test/utils-tests.js
+++ test/utils-tests.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var assert = require('assert');
 var utils = require('../lib/utils');
 var createStyleJsonFromString = utils.createStyleJsonFromString;
 
-describe('utils: createStyleJsonFromString', function () {
+describe('utils: createStyleJsonFromString', function() {
   it('works', function () {
     var input = 'display:block; margin:0; padding:0;';
     var output = createStyleJsonFromString(input);
@@ -14,12 +16,12 @@ describe('utils: createStyleJsonFromString', function () {
     });
   });
 
-  it('works for colons', function () {
-    var input = "background: url('data:image/png;base64,abcdef=/+') no-repeat";
+  it('works for colons', function() {
+    var input = 'background: url(\'data:image/png;base64,abcdef=/+\') no-repeat';
     var output = createStyleJsonFromString(input);
 
     assert.deepEqual(output, {
-      background: " url('data:image/png;base64,abcdef=/+') no-repeat"
+      background: ' url(\'data:image/png;base64,abcdef=/+\') no-repeat',
     });
   });
 });
