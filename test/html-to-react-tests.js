3472cbb1b0cdc278ec32e060552918b7c1401719
Gytis Alčiauskas <alciauskas@gmail.com>
2017-09-26 14:38:35 +0300

    Added trim on inline style parser.

--- test/html-to-react-tests.js
+++ test/html-to-react-tests.js
@@ -31,9 +31,9 @@ describe('Html2React', function () {
     });
 
     it('should return a valid HTML string with inline styles', function () {
-      var htmlInput = '<div style="background-image: url(' +
-        '&quot;http://lorempixel.com/400/200/&quot;);background-color: red;color: white;' +
-        'font-family: &quot;Open Sans&quot;;"></div>';
+      var htmlInput = '<div style="background-image:url(' +
+        '&quot;http://lorempixel.com/400/200/&quot;);background-color:red;color:white;' +
+        'font-family:&quot;Open Sans&quot;;"></div>';
 
       var reactComponent = parser.parse(htmlInput);
       var reactHtml = ReactDOMServer.renderToStaticMarkup(reactComponent);
@@ -42,9 +42,9 @@ describe('Html2React', function () {
     });
 
     it('should return a valid HTML string with inline image in style', function () {
-      var htmlInput = '<div style="background: url(' +
-        'data:image/png;base64,iVBORw0KGgoAAA);color: white;' +
-        'font-family: &quot;Open Sans&quot;;"></div>';
+      var htmlInput = '<div style="background:url(' +
+        'data:image/png;base64,iVBORw0KGgoAAA);color:white;' +
+        'font-family:&quot;Open Sans&quot;;"></div>';
 
       var reactComponent = parser.parse(htmlInput);
       var reactHtml = ReactDOMServer.renderToStaticMarkup(reactComponent);
