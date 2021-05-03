6ede5c2d69255717d6813604a1a02598795647af
Oswaldo Martinez Guillen <oswmargui50@hotmail.com>
2021-05-03 14:52:12 -0500

    Exposed camel case attribute names map

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const parser = require('./lib/parser');
 const processingInstructions = require('./lib/processing-instructions');
 const isValidNodeDefinitions = require('./lib/is-valid-node-definitions');
 const processNodeDefinitions = require('./lib/process-node-definitions');
+const camelCaseAttributeNamesMap = require('lib/camel-case-attribute-names');
 
 module.exports = {
   Parser: parser,
   ProcessingInstructions: processingInstructions,
   IsValidNodeDefinitions: isValidNodeDefinitions,
   ProcessNodeDefinitions: processNodeDefinitions,
+  camelCaseAttributeNamesMap,
 };
